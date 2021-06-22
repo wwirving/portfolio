1ffec47e4254519f2a960b712d73dfdca89b3533
Will Irving <willirving@Wills-Mac-mini.local>
2021-06-22 12:11:31 +0100

    improved fade in

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Menu from "../../components/Menu";
 import Slides from "../../components/Slides";
 import styles from "./Home.module.scss";
 
 const Home = () => {
+const [ animate, setAnimate ] = useState(true);
+
+const didMount = () => {
+  if (window.sessionStorage.getItem('firstLoadDone') === null){
+    setAnimate(true);
+    window.sessionStorage.setItem("firstLoadDone", 1);
+  } else {
+    setAnimate(false);
+  }
+}
+
+useEffect(() => {
+  console.log(window.sessionStorage.getItem('firstLoadDone'));
+  didMount();
+},[])
   return (
-    <div className={styles.mainContainer}>
+    <div className={animate ? styles.mainContainerFade : styles.mainContainer}>
       <Menu className={styles.mainmenu}></Menu>
       <Slides className={styles.slides}></Slides>
     </div>
