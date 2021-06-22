9ae05f8a8d037577af6952bfa7c25e7a054f0188
Will Irving <willirving@Wills-Mac-mini.local>
2021-06-22 13:54:46 +0100

    updated web descriptions

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,13 +1,14 @@
 const projects = [
   {
     title: "artefact",
+    menu: `Artefact`,
     tags: ["coding", "design"],
     mainImg:
       "https://drive.google.com/file/d/1nFQ4Px60qapHz0pgNRyve71UpfYWilD8/view?usp=sharing",
     alt: "artefact menu image",
     hoverImg: "",
     description:
-      `Artefact is a web API used to search and display images of artefacts from Met Museum archive , filtered by medium and search query. Click image to display more information`,
+      `Artefact is a web interface which can be used to search and display images of artefacts from the Metropolitan Museum of Art. Queries may be filtered by medium and/or keywords before pressing 'SEARCH' to return a collection of matching objects. Click images to display more information, or navigate through the search collection using the section at the bottom of page. The font used is 'Sporting Grotesque' by Lucas Le Bihan, George Triantafyllakos & Maciej Połczyński.`,
     links: {
       online: "https://artefact-api.web.app/",
       github: "https://github.com/wwirving/artefact",
@@ -19,19 +20,14 @@ const projects = [
   },
   {
     title: "objectCloud",
+    menu: `Object Cloud`,
     tags: ["coding", "design", "sound"],
     mainImg:
       "https://drive.google.com/file/d/1yqhG5S4u4a3H3XrB27OcPSBq9MWNCf7r/view?usp=sharing",
     alt: "objectCloud menu image",
     hoverImg: "../../assets/images/ocloud/main.png",
     description:
-      `Object Cloud is a browser based game built using Javascript, HTML and CSS. 
-
-The game is built using HTML canvas and a class orientated approach to game mechanics and design. Each object (sourced from gifs by ac-bu studio) has uniquely generated parameters and exists in an audio visual feedback-loop which increases throughout the game. 
-
-The audio events for the game are handled by Howler.JS and then processed globally through Tuna.JS, allowing global control of reverb and delay. Visual feedback is then modulated by increasing the canvas-refresh time within the animation loop. Game audio was synthesised and recorded in Ableton Live, with gamelan samples sourced from the Omnisphere library. 
-
-The main character and design of the game were generated in Figma. The font used is Overwatch. Though designed to be played on desktop, the game also scales well to mobile and can be played continously between mobile and portrait modes.`,
+      `Object Cloud is a browser based video game environment. Navigate around the game using mouse (or touch) and try to avoid objects for as long as possible. Each object (sourced from assets by AC-bu studio) has uniquely generated audio and visual parameters and exists in a feedback-loop which increases throughout the game and upon collision with the player. All sound for the game was recorded in Ableton Live and structured using Howler.JS. The font used is 'Outward' by Raoul Audouin.`,
     links: {
       online: "https://wwirving.github.io/objectCloud/",
       github: "https://github.com/wwirving/objectCloud",
@@ -43,10 +39,11 @@ The main character and design of the game were generated in Figma. The font used
   },
   {
     title: "webCalculator",
+    menu: `Digital Calculator`,
     tags: ["code", "design", "testing"],
     mainImg:
       "https://drive.google.com/file/d/1PRfcFofpdlX1_ymOcs7UFwDm2tsYZgtY/view?usp=sharing",
-    description: "Digital calculator created using HTML, CSS and JS",
+    description: "This is a basic digital calculator programmed using Javascript. Click the body of the calculator to change colour, screen to clear, and search through previous queries using the right hand panel. The font used is 'Basteleur' by Keussel.",
     links: {
       online: "https://wwirving.github.io/webCalculator/",
       github: "https://github.com/wwirving/webCalculator",
@@ -58,10 +55,11 @@ The main character and design of the game were generated in Figma. The font used
   },
   {
     title: "morseTranslator",
+    menu: `Morse Translator`,
     tags: ["code", "design", "testing"],
     mainImg:
       "https://drive.google.com/file/d/1YcQ55OuoyVpvIhLqyh2zpomU86vVJgH6/view?usp=sharing",
-    description: "",
+    description: "This is a translator and audio interface for translation of characters into Morse Code programmed using Javascript. Enter text and hit the circular button to translate, and make sure sound is enabled to hear the audio translation. Order can be reversed by clicking the left hand side button. The fonts used are 'Happy Times at the IKOB New Game Plus Edition' by Lucas Le Bihan and Pilowlava by Anton Moglia + Jérémy Landes.",
     links: {
       online: "https://wwirving.github.io/morseTranslator/",
       github: "https://github.com/wwirving/morseTranslator",
